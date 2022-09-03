318d3d08ed0f7428ffeed3de07de04cda170232c
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2022-09-03 12:29:57 -0300

    Adiciona botão limpar

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function clickButton() {
-  const button = document.getElementById('button-random-color');
-  button.addEventListener('click', assignNewColors);
+function clickPalette() {
+  const buttonColorBlack = document.getElementById('black');
+  buttonColorBlack.addEventListener('click', );
+
+  const buttonColor2 = document.getElementById('color2');
+  buttonColor2.addEventListener('click', );
+
+  const buttonColor3 = document.getElementById('color3');
+  buttonColor3.addEventListener('click', );
+
+  const buttonColor4 = document.getElementById('color4');
+  buttonColor4.addEventListener('click', );
 }
 
 function generateColor() {
@@ -39,7 +48,29 @@ function assignNewColors() {
   colors2.style.backgroundColor = JSON.parse(cA);
   colors3.style.backgroundColor = JSON.parse(cB);
   colors4.style.backgroundColor = JSON.parse(cC);
-
-
 }
+
+function colorSelection() {
+  clickPalette
+}
+
+function allWhite() {
+  const whitePx = document.getElementsByClassName('pixel');
+  for (let index = 0; index < whitePx.length; index += 1) {
+    whitePx[index].style.backgroundColor = 'rgb(255, 255, 255)';
+  }
+  return whitePx;
+}
+
+function clickButton() {
+  const button = document.getElementById('button-random-color');
+  button.addEventListener('click', assignNewColors);
+}
+
+function clearButton() {
+  const clearPx = document.getElementById('clear-board');
+  clearPx.addEventListener('click', allWhite);
+}
+
 clickButton();
+clearButton();
