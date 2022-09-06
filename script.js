b19643d0d9ef447d9aa0886eb4bd03197143343b
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2022-09-05 22:54:50 -0300

    Adiciona cores a local storage

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* It's generating a random color. */
 function generateColor() {
   function rgb(_color2, _color3, _color4) {
   }
@@ -12,28 +13,32 @@ function generateColor() {
   return newColor;
 }
 
+/* It's generating a random color. */
 function newPaletteColors() {
   const numCol = generateColor();
   const strCol = 'rgb(' + numCol[0] + ', ' + numCol[1] + ', ' + numCol[2] + ')';
   return strCol;
 }
-
+/* It's saving the color palette generated randomly. */
 function assignNewColors() {
-  let cA = '"' + newPaletteColors() + '"';
+  let cBl = JSON.stringify('rgb(0, 0, 0)');
+  const colorsBlack = document.getElementById('black');
+  let cA = JSON.stringify(newPaletteColors());
   const colors2 = document.getElementById('color2');
-  let cB = '"' + newPaletteColors() + '"';
+  let cB = JSON.stringify(newPaletteColors());
   const colors3 = document.getElementById('color3');
-  let cC = '"' + newPaletteColors() + '"';
+  let cC = JSON.stringify(newPaletteColors());
   const colors4 = document.getElementById('color4');
-
-  if (cA === cB === cC || cA === cB || cB === cC || cA === cC) {
-    cA = '"' + newPaletteColors() + '"';
-    cB = '"' + newPaletteColors() + '"';
-    cC = '"' + newPaletteColors() + '"';
+  if (cBl === cA === cB === cC || cA === cB || cB === cC || cA === cC) {
+    cA = JSON.stringify(newPaletteColors());
+    cB = JSON.stringify(newPaletteColors());
+    cC = JSON.stringify(newPaletteColors());
   }
+  colorsBlack.style.backgroundColor = JSON.parse(cBl);
   colors2.style.backgroundColor = JSON.parse(cA);
   colors3.style.backgroundColor = JSON.parse(cB);
   colors4.style.backgroundColor = JSON.parse(cC);
+  return [cBl, cA, cB, cC];
 }
 
 function removeSelected() {
@@ -80,7 +85,10 @@ function allWhite() {
 
 function clickButton() {
   const button = document.getElementById('button-random-color');
-  button.addEventListener('click', assignNewColors);
+  button.addEventListener('click', function () {
+    const newRandom = assignNewColors();
+    localStorage.setItem('colorPalette', JSON.stringify(newRandom));
+  });
 }
 
 function clearButton() {
@@ -102,6 +110,28 @@ function clickPalette() {
   buttonColor4.addEventListener('click', colorSelected4);
 }
 
+// Função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página;
+
+function loadColor() {
+  const localColor = JSON.parse(localStorage.getItem('colorPalette'));
+  const saveBlack = document.getElementsByClassName('color')[0];
+  saveBlack.style.backgroundColor = JSON.parse(localColor[0]);
+  const saveColor2 = document.getElementsByClassName('color')[1];
+  saveColor2.style.backgroundColor = JSON.parse(localColor[1]);
+  const saveColor3 = document.getElementsByClassName('color')[2];
+  saveColor3.style.backgroundColor = JSON.parse(localColor[2]);
+  const saveColor4 = document.getElementsByClassName('color')[3];
+  saveColor4.style.backgroundColor = JSON.parse(localColor[3]);
+  return localColor;
+}
+
+// Função que permite preencher um pixel do quadro com a cor selecionada na paleta de cores;
+
+// Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco;
+
+// Crie uma função para salvar e recuperar o seu desenho atual no localStorage;
+
 clickButton();
 clearButton();
 clickPalette();
+loadColor();
